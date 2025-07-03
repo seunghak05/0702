d6e44f0002d0d573a6af5928dab5eb05b9cb7ec8
DESKTOP-JIH7G9N\202_11 <wincrane05@gmail.com>
2025-07-03 09:40:42 +0900

    뭘 한지는 모르겠지만 뭔가 추가를 하긴 함

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class rotator : MonoBehaviour
 {
     public float rotationSpeed = 50f; // Speed of rotation in degrees per second
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+                                      // Start is called once before the first execution of Update after the MonoBehaviour is created
     
-
+     // 게임 오버 상태를 나타내는 변수
+    
     // Update is called once per frame
     void Update()
     {
+        // while (!gameover)
+        // {
+            
+        // }
         transform.Rotate(0f, rotationSpeed * Time.deltaTime,0f, Space.Self); // Rotate around the Y-axis
     }
 }
